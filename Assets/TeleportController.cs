aa93c459dbf50b1bfdc1af7c18844f9cc1117f84
DESKTOP-99KM3IT\inter <interaktiv99@yandex.ru>
2023-05-02 17:35:23 +0300

    Остановки при телепорте добавлена

--- Assets/TeleportController.cs
+++ Assets/TeleportController.cs
@@ -7,11 +7,15 @@ public class TeleportController : MonoBehaviour
 {
     [SerializeField] private GameObject questionCanvas;
     [SerializeField] private GameObject placeToTP;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            other.transform.position = placeToTP.transform.position;
+            Rigidbody playerRb = other.GetComponent<Rigidbody>();
+            playerRb.MovePosition(placeToTP.transform.position);
+            playerRb.velocity = Vector3.zero; // установить скорость игрока равной нулю
+            playerRb.angularVelocity = Vector3.zero;
             questionCanvas.SetActive(true);
         }
     }
